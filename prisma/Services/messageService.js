8a362c2bb0ee177380ba3c130f9e026154224ead
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-07-25 21:31:05 +0300

    Define message queries

---
+++ prisma/Services/messageService.js
@@ -0,0 +1,64 @@
+const prisma = require("../prisma");
+
+async function createMessage(message) {
+  try {
+    return await prisma.message.create({
+      data: {
+        text: message.text,
+        image: message.image,
+        authorId: message.authorId,
+        receiverId: message.receiverId,
+        groupId: message.groupId,
+      },
+    });
+  } catch (err) {
+    throw new Error(`DB: could not create message. Error:${err}`);
+  }
+}
+
+async function getMessages(firstUser, secondUser) {
+  try {
+    return await prisma.message.findFirst({
+      where: {
+        OR: [
+          {
+            authorId: firstUser,
+            receiverId: secondUser,
+          },
+          {
+            authorId: secondUser,
+            receiverId: firstUser,
+          },
+        ],
+      },
+
+      orderBy: { createdAt: "asc" },
+    });
+  } catch (err) {
+    throw new Error(`DB: could not fetch messages. Error:${err}`);
+  }
+}
+
+async function editMessage(message) {
+  try {
+    return await prisma.message.update({
+      where: { id: message.id },
+      data: {
+        text: message.text,
+        image: message.text,
+      },
+    });
+  } catch (err) {
+    throw new Error(`DB: could not edit message. Error:${err}`);
+  }
+}
+
+async function getMessageById(id) {
+  try {
+    return await prisma.message.findUnique({ where: { id } });
+  } catch (err) {
+    throw new Error(`DB: could not fetch message. Error:${err}`);
+  }
+}
+
+module.exports = { createMessage, getMessages, editMessage, getMessageById };
--- prisma/queries.js
+++ prisma/queries.js
@@ -1,4 +1,5 @@
 const user = require("./Services/userService");
 const profile = require("./Services/profileService");
+const message = require("./Services/messageService");
 
-module.exports = { user, profile };
+module.exports = { user, profile, message };
